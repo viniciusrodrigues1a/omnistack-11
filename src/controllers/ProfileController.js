2500e9224a0943adc3afa442755dfc30b2926cda
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-27 03:42:15 -0300

    :sparkles: controller dos incidents de uma ong específica

---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,17 @@
+import connection from '../database/connection';
+
+export default {
+  async index(req, res) {
+    const ong_id = req.headers.authorization;
+
+    if (!ong_id) {
+      return res.status(401).json({ error: 'Operation not permitted.' });
+    }
+
+    const incidents = await connection('incidents')
+      .where('ong_id', ong_id)
+      .select('*');
+
+    return res.json(incidents);
+  },
+};
