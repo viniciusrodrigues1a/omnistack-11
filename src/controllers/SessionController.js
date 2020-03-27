461ab773ad84f74edc01dda50a337b467f51e342
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-27 03:42:25 -0300

    :sparkles: controller de sessão

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,15 @@
+import connection from '../database/connection';
+
+export default {
+  async create(req, res) {
+    const { id } = req.body;
+
+    const ong = await connection('ongs').where('id', id).select('name').first();
+
+    if (!ong) {
+      return res.status(400).json({ error: 'No ONG found with this ID.' });
+    }
+
+    return res.json(ong);
+  },
+};
