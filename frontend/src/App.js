d111cf359d11ee7a4a15f36dad39d6972bcd3871
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-28 05:37:08 -0300

    :sparkles: adicionando o react-toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import Routes from './routes';
+import './styles/toastStyles.css';
 import GlobalStyles from './styles/globalStyles';
+
+toast.configure({
+  autoClose: 4000,
+  draggable: false,
+  position: toast.POSITION.TOP_RIGHT,
+});
 
 function App() {
   return (
