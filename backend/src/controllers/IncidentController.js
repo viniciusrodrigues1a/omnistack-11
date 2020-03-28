a1cd78496e7aacacb2daecb1c06ec68cae61230b
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-28 01:21:30 -0300

    :sparkles: verificando autenticação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -39,6 +39,10 @@ export default {
     const { title, description, value } = req.body;
     const ong_id = req.headers.authorization;
 
+    if (!ong_id) {
+      return res.status(401).json({ error: 'Operation not permitted.' });
+    }
+
     const [id] = await connection('incidents').insert({
       title,
       description,
