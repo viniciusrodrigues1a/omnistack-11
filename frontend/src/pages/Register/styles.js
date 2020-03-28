e4347ac9d2a7abbd49cd4ba101d805b25ef3a882
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-28 06:16:59 -0300

    :lipstick: estilizando o conteúdo do toast de sucesso da página de Logon

--- frontend/src/pages/Register/styles.js
+++ frontend/src/pages/Register/styles.js
@@ -1,1 +1,29 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const NotifyToastContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'Roboto', sans-serif;
+
+  p:first-child {
+    padding-bottom: 0.5rem;
+    margin-bottom: 0.5rem;
+    border-bottom: 1px solid #ffffff44;
+  }
+
+  p {
+    font-size: 1.5em;
+    color: #fff;
+
+    span {
+      font-size: 1.25em;
+      font-weight: bold;
+    }
+  }
+
+  button {
+    border: 1px solid ${darken(0.12, '#e02041')};
+  }
+`;
