80297fe240c2129604ff2e30a246387be3ed2e86
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-29 07:27:55 -0300

    :wrench: pegando a url da api de dentro do .env

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from 'react-native-dotenv';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.113:3333',
+  baseURL: API_URL,
 });
 
 export default api;
