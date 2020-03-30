a99b88268358886ba831009aaad5199d32d82e55
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-30 10:04:10 -0300

    :sparkles: passando api do heroku

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { API_URL } from 'react-native-dotenv';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: 'https://be-the-hero-omnistack-backend.herokuapp.com/',
 });
 
 export default api;
