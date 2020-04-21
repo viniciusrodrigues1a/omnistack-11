fecfa26fe9d45bf056f6edbbbeda19b8b10dc287
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-21 13:43:02 -0300

    :sparkles: utilizando redux-persist

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  persistReducer({ key: 'bethehero', storage }, rootReducer),
+  applyMiddleware(sagaMiddleware)
+);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
